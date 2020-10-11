4f8f59f56061a4bea5db8debccc9d50136992675
suerfu <suerfu64@gmail.com>
2020-10-11 12:16:32 -0700

    * LZ upper truss implemented based on the design file from 2017.

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -7,10 +7,13 @@
 #include "globals.hh"
 #include "G4VUserDetectorConstruction.hh"
 #include "G4Cache.hh"
-#include "OxCryoMaterials.hh"
 #include "G4ThreeVector.hh"
 #include "tls.hh"
 #include "G4RotationMatrix.hh"
+#include "G4VPVParameterisation.hh"
+#include "G4PVParameterised.hh"
+
+#include "G4ExtrudedSolid.hh"
 
 class G4VPhysicalVolume;
 class G4LogicalVolume;
@@ -55,18 +58,51 @@ private:
     G4double world_y;
     G4double world_z;
 
-    // Experimental chamber
-    G4double chamber_h;
-
-    // Various shielding
-    G4double shielding_Cu;
-    G4double shielding_PE;
-    G4double shielding_Pb;
-    G4double shielding_SF;
-
 };
 
 
+class TrussParameterisation : public G4VPVParameterisation {
+
+public:
+    TrussParameterisation( ){}
+
+    virtual ~TrussParameterisation(){}
+
+    void ComputeTransformation( const G4int copyNo, G4VPhysicalVolume* physVol) const;
+
+    void ComputeDimensions (G4ExtrudedSolid&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+
+private:  // Dummy declarations to get rid of warnings ...
+
+    void ComputeDimensions (G4Box&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions( G4Tubs&,const G4int,
+                            const G4VPhysicalVolume* ) const{}
+    void ComputeDimensions (G4Trd&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Trap&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Cons&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Sphere&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Orb&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Ellipsoid&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Torus&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Para&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Hype&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Polycone&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+    void ComputeDimensions (G4Polyhedra&,const G4int,
+                            const G4VPhysicalVolume*) const {}
+
+};
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #endif
--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -11,18 +11,18 @@
 #include "G4NistManager.hh"
 
 #include "DetectorConstructionMessenger.hh"
+#include <sstream>
 
 #include "G4Box.hh"
 #include "G4Tubs.hh"
 #include "G4Cons.hh"
-#include "G4Orb.hh"
-#include "G4ExtrudedSolid.hh"
 #include "G4SubtractionSolid.hh"
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4GlobalMagFieldMessenger.hh"
 #include "G4AutoDelete.hh"
 #include "G4VisExtent.hh"
+#include "G4TwoVector.hh"
 
 #include "G4GeometryManager.hh"
 #include "G4PhysicalVolumeStore.hh"
@@ -45,7 +45,6 @@
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-
 DetectorConstruction::DetectorConstruction() : G4VUserDetectorConstruction() {
     fCheckOverlaps = true;
     fDetectorMessenger = new DetectorConstructionMessenger(this);
@@ -53,18 +52,9 @@ DetectorConstruction::DetectorConstruction() : G4VUserDetectorConstruction() {
     // Set default values for the dimensions.
     
     // World
-    world_x = 11.*m;
-    world_y = 12.*m;
-    world_z = 3.2*m;
-
-    // Experimental chamber
-    chamber_h = 30.*cm;
-
-    // Copper shielding
-    shielding_Cu = 6*cm;
-    shielding_PE = 20*cm;
-    shielding_Pb = 30*cm;
-    shielding_SF = 5*cm;
+    world_x = 10.*m;
+    world_y = 10.*m;
+    world_z = 10*m;
 
 }
 
@@ -104,6 +94,7 @@ G4VPhysicalVolume* DetectorConstruction::DefineVolumes(){
 
     G4Material* world_material = mat_man->FindOrBuildMaterial("G4_Galactic");
     G4Material* frame_material = mat_man->FindOrBuildMaterial("G4_STAINLESS-STEEL");
+    G4Material* utruss_hole_material = world_material;
 
     G4Material* Pb_material = mat_man->FindOrBuildMaterial("G4_Pb");
     G4Material* PE_material = mat_man->FindOrBuildMaterial("G4_POLYETHYLENE");
@@ -119,20 +110,128 @@ G4VPhysicalVolume* DetectorConstruction::DefineVolumes(){
     G4LogicalVolume* world_lv = new G4LogicalVolume( world_solid, world_material, "world_lv");
     G4VPhysicalVolume* world_pv = new G4PVPlacement( 0, G4ThreeVector(0,0,0), world_lv, "world_pv", 0, false, 0,fCheckOverlaps);
 
+    G4double in = 2.54*mm;
 
-    // Support Frame
-    G4double frame_z = chamber_h+2*shielding_Cu+2*shielding_PE+2*shielding_Pb+2*shielding_SF;
-    G4double frame_x = frame_z;
-    G4double frame_y = frame_z;
+    G4double utruss_thickness = 12.7*in;
 
-    G4ThreeVector frame_offset( 0, 0, -world_z/2+frame_z/2+10*cm);
-        // The additional 10 cm account for the rail system
+    G4ThreeVector utruss_origin(0,0,0);
+        // This is the origin of the truss frame (lower-left corner of the right half) in the global coordinate.
 
-    G4Box* frame_solid = new G4Box( "frame_solid", frame_x/2.0, frame_y/2.0, frame_z/2.0);
-    G4LogicalVolume* frame_lv = new G4LogicalVolume( frame_solid, frame_material, "frame_lv");
-    new G4PVPlacement( 0, frame_offset, frame_lv, "frame_pv", world_lv, false, 0,fCheckOverlaps);
+    // Upper Truss
+    for( int angle=0; angle<6; angle++ ){
 
+        std::vector<G4TwoVector> utruss_polygon;
+        utruss_polygon.push_back( G4TwoVector( sqrt(3)*utruss_thickness/2, 0) );
+            // This is the origin. 
+            
+        utruss_polygon.push_back( G4TwoVector( 72*in, 0) );
+        utruss_polygon.push_back( G4TwoVector( 72*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 72*in+16*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 72*in+16*in, 0) );
+            // first extruded part
 
+        utruss_polygon.push_back( G4TwoVector( 224*in, 0) );
+        utruss_polygon.push_back( G4TwoVector( 224*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 224*in+32*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 224*in+32*in, 0) );
+
+        utruss_polygon.push_back( G4TwoVector( 412*in, 0) );
+        utruss_polygon.push_back( G4TwoVector( 412*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 412*in+16*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 412*in+16*in, 0) );
+
+        utruss_polygon.push_back( G4TwoVector( 517*in, 0) );
+        utruss_polygon.push_back( G4TwoVector( 517*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 517*in+32*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 517*in+32*in, 0) );
+
+        utruss_polygon.push_back( G4TwoVector( 592*in, 0) );
+        utruss_polygon.push_back( G4TwoVector( 592*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 592*in+16*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 592*in+16*in, 0) );
+        
+        utruss_polygon.push_back( G4TwoVector( 719.8*in, 0) );
+        utruss_polygon.push_back( G4TwoVector( 719.8*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 774.67*in-10.17*in, -18*in) );
+        utruss_polygon.push_back( G4TwoVector( 774.67*in-10.17*in, -56*in) );
+        utruss_polygon.push_back( G4TwoVector( 774.67*in, -56*in) );
+        
+        utruss_polygon.push_back( G4TwoVector( 774.67*in, 107.95*in-74.31*in) );
+            // Here the 74.31 should be changed based on the actual geometry not specified in the drawing
+        utruss_polygon.push_back( G4TwoVector( 101.6*in, 107.95*in) );
+        //utruss_polygon.push_back( G4TwoVector( 44.45*in, 107.95*in) );
+        //utruss_polygon.push_back( G4TwoVector( 44.45*in, 107.95*in+15.88*in) );
+        //utruss_polygon.push_back( G4TwoVector( sqrt(3)*utruss_thickness/2, 107.95*in+15.88*in) );
+            // The above part will be integrated into the cylinder part.
+        
+        utruss_polygon.push_back( G4TwoVector( sqrt(3)*utruss_thickness/2, 107.95*in) );
+
+        std::vector<G4ExtrudedSolid::ZSection> utruss_zsections;
+        utruss_zsections.push_back( G4ExtrudedSolid::ZSection( -utruss_thickness/2 , G4TwoVector(0,0), 1.0) );
+        utruss_zsections.push_back( G4ExtrudedSolid::ZSection( utruss_thickness/2 , G4TwoVector(0,0), 1.0) );
+        
+        G4ExtrudedSolid* utruss_solid = new G4ExtrudedSolid( "utruss_solid", utruss_polygon, utruss_zsections );
+        
+        G4LogicalVolume* utruss_lv = new G4LogicalVolume( utruss_solid, frame_material, "utruss_lv");
+        G4RotationMatrix* rot = new G4RotationMatrix();
+        rot->rotateX(-90.*deg );
+        rot->rotateY( 60.*deg*angle );
+        
+        std::stringstream ss;
+        ss << "_" << angle;
+        new G4PVPlacement( rot, utruss_origin, utruss_lv, "utruss_pv"+ss.str(), world_lv, false, 0,fCheckOverlaps);
+
+        //G4VPVParameterisation* utruss_param = 
+        //    new TrussParameterisation( );
+
+        //new G4PVParameterised("utruss", frame_lv, world_lv, kZAxis, 6, utruss_param, fCheckOverlaps);
+
+        std::vector<G4ThreeVector> utruss_hole_pos;
+            // location of upper truss hole positions.
+            // the z position is used as radius
+        utruss_hole_pos.push_back( G4ThreeVector( 92.08*in,  51.85*in, 50.8*in/2 ) );
+        utruss_hole_pos.push_back( G4ThreeVector( 240*in,    51.85*in, 50.8*in/2 ) );
+        utruss_hole_pos.push_back( G4ThreeVector( 371.43*in, 42.95*in, 38.1*in/2 ) );
+        utruss_hole_pos.push_back( G4ThreeVector( 533*in,    32.00*in, 25.4*in/2 ) );
+        utruss_hole_pos.push_back( G4ThreeVector( 472.5*in,  20.10*in, 12.7*in/2 ) );
+        utruss_hole_pos.push_back( G4ThreeVector( 600.5*in,  20.10*in, 12.7*in/2 ) );
+        utruss_hole_pos.push_back( G4ThreeVector( 663.5*in,  20.10*in, 12.7*in/2 ) );
+        utruss_hole_pos.push_back( G4ThreeVector( 750.0*in,  20.10*in, 12.7*in/2 ) );
+
+        for( unsigned int i=0; i<utruss_hole_pos.size(); i++){
+            ss.clear();
+            ss << angle << "_" << i;
+            G4Tubs* hole_solid = new G4Tubs( "utruss_hole_sld_"+ss.str(), 0, utruss_hole_pos[i].z(), utruss_thickness/2, 0, CLHEP::twopi );
+            G4LogicalVolume* hole_lv = new G4LogicalVolume( hole_solid, utruss_hole_material, G4String("utruss_hole_lv_")+ss.str() );
+            G4ThreeVector hole_pos( utruss_hole_pos[i].x(), utruss_hole_pos[i].y() );
+            new G4PVPlacement( 0, hole_pos, hole_lv, "utruss_lv"+ss.str(), utruss_lv, false, 0, fCheckOverlaps );
+        }
+    }
+
+    std::vector<G4TwoVector> utruss_center_polygon;
+    utruss_center_polygon.push_back( G4TwoVector( sqrt(3)*utruss_thickness/2, utruss_thickness/2) );
+    utruss_center_polygon.push_back( G4TwoVector( 0, utruss_thickness) );
+    utruss_center_polygon.push_back( G4TwoVector( -sqrt(3)*utruss_thickness/2, utruss_thickness/2) );
+    utruss_center_polygon.push_back( G4TwoVector( -sqrt(3)*utruss_thickness/2, -utruss_thickness/2) );
+    utruss_center_polygon.push_back( G4TwoVector( 0, -utruss_thickness) );
+    utruss_center_polygon.push_back( G4TwoVector( sqrt(3)*utruss_thickness/2, -utruss_thickness/2) );
+
+    std::vector<G4ExtrudedSolid::ZSection> utruss_center_zsections;
+    utruss_center_zsections.push_back( G4ExtrudedSolid::ZSection( 0 , G4TwoVector(0,0), 1.0) );
+    utruss_center_zsections.push_back( G4ExtrudedSolid::ZSection( 107.95*in , G4TwoVector(0,0), 1.0) );
+        
+    G4ExtrudedSolid* utruss_center_solid = new G4ExtrudedSolid( "utruss_center_solid", utruss_center_polygon, utruss_center_zsections );
+    G4LogicalVolume* utruss_center_lv = new G4LogicalVolume( utruss_center_solid, frame_material, "utruss_center_lv");
+    new G4PVPlacement( 0, utruss_origin, utruss_center_lv, "utruss_center", world_lv, false, 0, fCheckOverlaps);
+
+    G4double utruss_cap_thickness = 15.88*in;
+    G4Tubs* utruss_cap_solid = new G4Tubs( "utruss_cap", 0, 101.60*in, utruss_cap_thickness/2, 0, CLHEP::twopi);
+    G4LogicalVolume* utruss_cap_lv = new G4LogicalVolume( utruss_cap_solid, frame_material, "utruss_cap_lv");
+
+    G4ThreeVector utruss_cap_offset = utruss_origin + G4ThreeVector(0,0,107.95*in+utruss_cap_thickness/2);
+    new G4PVPlacement( 0, utruss_cap_offset, utruss_cap_lv, "utruss_cap", world_lv, false, 0, fCheckOverlaps);
+
+    /*
     // Lead shielding
     G4double Pb_z = chamber_h+2*shielding_Cu+2*shielding_PE+2*shielding_Pb;
     G4double Pb_x = Pb_z;
@@ -175,13 +274,14 @@ G4VPhysicalVolume* DetectorConstruction::DefineVolumes(){
 
     //===============  Visualization ===============//
 
-    world_lv->SetVisAttributes( G4VisAttributes::Invisible );
 
-    frame_lv->SetVisAttributes( G4VisAttributes(G4Colour( 0, 0, 1, 0.5)) );
     Pb_lv->SetVisAttributes( G4VisAttributes(G4Colour( 0.5, 0.5, 0.5, 0.5)) );
     PE_lv->SetVisAttributes( G4VisAttributes(G4Colour( 0.9, 0.9, 0.9, 0.5)) );
     Cu_lv->SetVisAttributes( G4VisAttributes(G4Colour( 0.9, 0.4, 0., 0.5)) );
     chamber_lv->SetVisAttributes( G4VisAttributes(G4Colour( 0.9, 0., 0.9, 0.5)) );
+    */
+    world_lv->SetVisAttributes( G4VisAttributes::Invisible );
+    //utruss_lv->SetVisAttributes( G4VisAttributes(G4Colour( 0.5, 0.5, 0.5, 0.5)) );
 
     return world_pv;
 }
@@ -210,5 +310,15 @@ void DetectorConstruction::DefineMaterials(){
     // Print materials
     G4cout << *(G4Material::GetMaterialTable()) << G4endl;
 }
+
+
+
+void TrussParameterisation::ComputeTransformation( const G4int copyNo, G4VPhysicalVolume* physVol) const{
+        physVol->SetTranslation( G4ThreeVector(0,0,0));
+        G4RotationMatrix* rot = new G4RotationMatrix();
+        rot->rotateX(-90.*deg);
+        rot->rotateY(-60.*deg*copyNo);
+        physVol->SetRotation( rot );
+    }
 
 
