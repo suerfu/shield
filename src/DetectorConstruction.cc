f5b0aded162e11aeca7399e62bf346dab030f4ee
Burkhant Suerfu <suerfu@berkeley.edu>
2021-02-04 09:23:20 -0800

    Added code for regular copper shielding instead of NaI

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -43,7 +43,7 @@ DetectorConstruction::DetectorConstruction() : G4VUserDetectorConstruction() {
     world_z = 3.2*m;
 
     offset = 5*cm;
-
+/*
     // Constructor format: Name, Material, Largest dim, Weight, No. of layers.
     list.push_back( ShieldInfo("frame", "G4_STAINLESS-STEEL",   132*cm, 1   ) );
     list.push_back( ShieldInfo("Pb",    "G4_Pb",                122*cm, 1, 1   ) );
@@ -51,7 +51,15 @@ DetectorConstruction::DetectorConstruction() : G4VUserDetectorConstruction() {
     list.push_back( ShieldInfo("PE",    "G4_POLYETHYLENE",      82*cm,  1   ) );
         // PE should be 122 102 82 for 10 20 and 30 cm of lead.
     list.push_back( ShieldInfo("Cu",    "G4_Cu",                42*cm,  1, 1   ) );
-    list.push_back( ShieldInfo("veto",  "NaI",                  30*cm,  1   ) );
+    list.push_back( ShieldInfo("veto",  "G4_Cu",                30*cm,  1   ) );
+    list.push_back( ShieldInfo("chamber",    "liquid_helium",   20*cm,  1   ) );
+*/
+
+    list.push_back( ShieldInfo("frame", "G4_STAINLESS-STEEL",   132*cm, 1   ) );
+    list.push_back( ShieldInfo("Pb",    "G4_Pb",                122*cm, 2, 10  ) );
+    list.push_back( ShieldInfo("PE",    "G4_POLYETHYLENE",      82*cm,  1   ) );
+    list.push_back( ShieldInfo("Cu",    "G4_Cu",                42*cm,  2, 2   ) );
+    list.push_back( ShieldInfo("veto",  "G4_Cu",                30*cm,  1   ) );
     list.push_back( ShieldInfo("chamber",    "liquid_helium",   20*cm,  1   ) );
 
     list = ExpandList( list );
